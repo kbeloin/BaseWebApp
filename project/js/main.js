08aa2eb806387dc822922699aca86b223d3ab8c3
kbeloin <61366520+kbeloin@users.noreply.github.com>
2021-05-14 16:16:30 -0700

    fixed api call

--- project/js/main.js
+++ project/js/main.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 
 function getSubscriptions(){
   var userurl = 'https://api.hubapi.com/crm/v3/schemas/2-1413617';
-  var data = {hapikey: apiKey};
+  var userdata = {hapikey: apiKey};
   
   $.ajax({
     method: "GET",
-    url: url,
+    url: userurl,
     data: userdata,
     success: function(data) {
       console.log(data);
