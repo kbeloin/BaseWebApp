fd3add5a41f2082c07c9b3c268f777018a8fe89f
kbeloin <61366520+kbeloin@users.noreply.github.com>
2021-05-14 15:54:26 -0700

    Made change

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,25 @@
+$(document).ready(function(){
+  getSubscriptions();
+})
+
+function getSubscriptions(){
+  var request = require("request");
+
+  var options = {
+    method: 'GET',
+    url: 'https://api.hubapi.com/crm/v3/schemas/2-1413617',
+    qs: {hapikey: apiKey},
+    headers: {accept: 'application/json'}
+  };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
+}
+
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
